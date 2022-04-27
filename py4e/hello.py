d9a517111b3bbfcf83c22f4878649a66f253b497
William <wmdragonfire@gmail.com>
2022-04-27 04:08:48 -0500

    hello.py

---
+++ py4e/hello.py
@@ -0,0 +1,11 @@
+print('Hello World')
+x = 1 + 2 ** 3 / 4 * 5
+print(x)
+
+print(float(99) + 100) # 199.0
+i = 42
+print(i)
+print(type(i)) # <class 'int'>
+f = float(i)
+print(f) # 42.0
+print(type(f)) # <class 'float'>
